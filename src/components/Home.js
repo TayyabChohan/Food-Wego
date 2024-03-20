241ee6ef35e8c3762c11eac1d706887688146292
TayyabChohan <TayyabChohan7@gmail.com>
2024-03-20 23:03:30 +0500

    completed the category filter

--- src/components/Home.js
+++ src/components/Home.js
@@ -26,21 +26,24 @@ const Home = ({ selectedCategory }) => {
     }, 2000); // Simulated loading delay
     setIsLoadingMore(false);
   };
-  useEffect(() => {
-    if (selectedCategory) {
-      const filteredData = isfoodProductList.filter(
-        (i) => i.categoryId === selectedCategory
-      );
-      setFoodProductList(filteredData);
-      console.log(filteredData,isfoodProductList);
-    }
-  }, [selectedCategory]);
+  // useEffect(() => {
+  //   if (selectedCategory) {
+  //     const filteredData = isfoodProductList.filter(
+  //       (i) => i.categoryId === selectedCategory
+  //     );
+  //     setFoodProductList(filteredData);
+  //     console.log(filteredData,isfoodProductList);
+  //   }
+  // }, [selectedCategory]);
+  const filteredData = selectedCategory
+    ? isfoodProductList.filter((i) => i.categoryId === selectedCategory)
+    : isfoodProductList;
   return (
     <>
       <main>
-        <Card isfoodProductList={isfoodProductList.slice(0, visibleCount)} />
+        <Card isfoodProductList={filteredData.slice(0, visibleCount)} />
       </main>
-      {visibleCount < isfoodProductList.length && (
+      {visibleCount < filteredData.length && (
         <LoadMoreButton
           onLoadMore={handleLoadMore}
           isLoadingMore={isLoadingMore}
